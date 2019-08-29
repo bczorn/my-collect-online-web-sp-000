d8c00893374efcf77fc2dcf2238c02ec1ab3ccd9
Becky Zorn <bczorn@gmail.com>
2019-08-29 04:21:24 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,16 @@
 def my_collect(array)
-  
+  i = 0
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+my_collect(languages) do |language|
+  language.upcase
+end
+
+my_collect(students) do |student|
+  student.split(" ").first
 end
